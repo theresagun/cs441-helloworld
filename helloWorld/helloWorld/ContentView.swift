cdf88faf3f84c2d56cdb2c8c04be5809fafd5bd3
Theresa Gundel <tgundel1@binghamton.edu>
2021-02-15 18:35:04 -0500

    started new app

---
+++ helloWorld/helloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloWorld
+//
+//  Created by Theresa Gundel on 2/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloWorld/helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Theresa Gundel on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
