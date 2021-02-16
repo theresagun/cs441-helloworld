d9139ab11738b281802d38a4c0a63c7fa4cee8b8
Theresa Gundel <tgundel1@binghamton.edu>
2021-02-15 20:24:14 -0500

    buttons to change the language

--- helloWorld/helloWorld/ViewController.m
+++ helloWorld/helloWorld/ViewController.m
@@ -12,11 +12,36 @@
 @end
 
 @implementation ViewController
+@synthesize mylabel;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)clickedEnglish:(id)sender
+{
+    [mylabel setText:@"Hello World!"];
+}
+
+- (IBAction)clickedFrench:(id)sender
+{
+    [mylabel setText:@"Bonjour Monde!"];
+}
+
+- (IBAction)clickedGerman:(id)sender
+{
+    [mylabel setText:@"Hallo Welt!"];
+}
+
+- (IBAction)clickedChinese:(id)sender
+{
+    [mylabel setText:@"你好世界!"];
+}
+
+- (IBAction)clickedLatin:(id)sender
+{
+    [mylabel setText:@"Salve Terra!"];
+}
 
 @end
